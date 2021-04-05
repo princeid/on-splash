2a466f53d3f201dbae90280ada100e047fe83c82
Prince Dickson <dicksoniprince@gmail.com>
2021-04-05 20:22:55 +0100

    setup routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import {Link, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/"><Photos/></Route>
+        <Route path="/cart"><Cart/></Route>
+      </Switch>
+      <h1>Home Page</h1>
     </div>
   );
 }
